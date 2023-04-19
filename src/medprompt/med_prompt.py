5dcdbb0ef08a5a70315e80e02030e0edfa46219a
Bell Eapen <github_public@gulfdoctor.net>
2023-04-19 21:03:11 +0000

    update med_prompt.py

--- src/medprompt/med_prompt.py
+++ src/medprompt/med_prompt.py
@@ -8,8 +8,6 @@ class MedPrompter(object):
         self,
         template_name: str = "default.jinja",
         template_path: str = None,
-        allowed_missing_variables: Optional[List[str]] = None,
-        # default_variable_values: Optional[Dict[str, Any]] = None,
         ):
         if template_path is None:
             self.template_path = resource_filename(__name__, "templates")
@@ -19,30 +17,12 @@ class MedPrompter(object):
         self.template_name = template_name
         self.env = Environment(loader=FileSystemLoader(self.template_path))
         self.template = self.env.get_template(self.template_name)
-        self.ast = self.env.parse(self.template.render())
-        self.variables = meta.find_undeclared_variables(self.ast)
-        self.allowed_missing_variables = allowed_missing_variables or [
-            "examples",
-            "description",
-            "output_format",
-        ]
-        # self.default_variable_values = default_variable_values or {}
-
 
     def list_templates(self) -> List[str]:
         return self.env.list_templates()
 
-
     def generate_prompt(self, variables: Dict[str, Any]) -> str:
-        # self.update_template_variables(variables)
         return self.template.render(variables)
 
-    def get_template_variables(self) -> List[str]:
-        return self.variables
 
-    def get_template_ast(self) -> Dict[str, Any]:
-        return self.ast
-
-    def get_template_ast_as_json(self) -> str:
-        return self.env.dump(self.ast)
 
