fdcba127933c3426bab2f42ff761ab7e593d5632
dermatologist <github@gulfdoctor.net>
2023-11-10 15:33:25 -0500

    Fix timestamp issue in get_time_diff_from_today function

--- src/medprompt/helpers.py
+++ src/medprompt/helpers.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta, timezone
 def get_time_diff_from_today(timestamp, datetime_format="%Y-%m-%dT%H:%M:%S%z"):
     """Return the difference between the given timestamp and today's date."""
     if len(timestamp) < 12:
-        timestamp += "T00:00:00+00:00"
+        timestamp += "T01:01:01+01:00"  #"2013-04-02T09:30:10+01:00"
     datetime_object = datetime.strptime(timestamp, datetime_format)
     datetime_object = datetime_object.replace(tzinfo=timezone.utc)
     return (datetime.now(timezone.utc) - datetime_object).days
