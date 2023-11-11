65cebc3a85b9b128df0a4cb18d5c58e81bef8cf6
Bell Eapen <github@gulfdoctor.net>
2023-11-11 12:35:17 -0500

    Update get_time_diff_from_today function and add patient_v1 template (Before auto)

--- src/medprompt/helpers.py
+++ src/medprompt/helpers.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 
 
-def get_time_diff_from_today(timestamp, datetime_format="%Y-%m-%dT%H:%M:%S%z", return_type="days"):
+def get_time_diff_from_today(timestamp, datetime_format="%Y-%m-%dT%H:%M:%S%z", return_type="auto"):
     """Return the difference between the given timestamp and today's date."""
     if len(timestamp) < 12:
         timestamp += "T01:01:01+01:00"  #"2013-04-02T09:30:10+01:00"
@@ -14,12 +14,12 @@ def get_time_diff_from_today(timestamp, datetime_format="%Y-%m-%dT%H:%M:%S%z", r
     elif return_type == "hours":
         return (datetime.now(timezone.utc) - datetime_object).seconds / 3600
     elif return_type == "days":
-        return (datetime.now(timezone.utc) - datetime_object).days
+        return int((datetime.now(timezone.utc) - datetime_object).days)
     elif return_type == "weeks":
-        return (datetime.now(timezone.utc) - datetime_object).days / 7
+        return int((datetime.now(timezone.utc) - datetime_object).days / 7)
     elif return_type == "months":
-        return (datetime.now(timezone.utc) - datetime_object).days / 30
+        return int((datetime.now(timezone.utc) - datetime_object).days / 30)
     elif return_type == "years":
-        return (datetime.now(timezone.utc) - datetime_object).days / 365
+        return int((datetime.now(timezone.utc) - datetime_object).days / 365)
     else:
-        return (datetime.now(timezone.utc) - datetime_object).days
+        return int((datetime.now(timezone.utc) - datetime_object).days)
