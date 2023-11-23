6df762c0abe29228e9adbf4069e1b3e247289355
Bell Eapen <github@gulfdoctor.net>
2023-11-23 17:56:47 -0500

    Update license headers in code files

--- src/medprompt/tools/fhir_to_text.py
+++ src/medprompt/tools/fhir_to_text.py
@@ -1,3 +1,20 @@
+"""
+ Copyright 2023 Bell Eapen
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+
 from typing import Optional, Type
 
 from fhir.resources.bundle import Bundle
@@ -5,9 +22,9 @@ from langchain.callbacks.manager import (AsyncCallbackManagerForToolRun,
                                          CallbackManagerForToolRun)
 from langchain.tools import BaseTool
 from pydantic import BaseModel, Field
+
 from src.medprompt import MedPrompter, get_time_diff_from_today
-from fhir.resources.bundle import Bundle
-from pydantic import BaseModel, Field
+
 
 class BundleInput(BaseModel):
     bundle_input: Bundle = Field()
@@ -31,9 +48,9 @@ class ConvertFhirToTextTool(BaseTool):
         output = ""
         for entry in bundle_input.entry:
             resource = entry.resource
-            if resource.resource_type == "Patient" or resource.resource_type == "Observation" \
+            if resource.resource_type == "Patient" or resource.resource_type == "AllergyIntolerance" \
                 or resource.resource_type == "Condition" or resource.resource_type == "Procedure" \
-                or resource.resource_type == "MedicationRequest" or resource.resource_type == "DiagnosticReport":
+                or resource.resource_type == "MedicationRequest":
                 obj: dict = resource.dict()
                 obj["time_diff"] = get_time_diff_from_today
                 output += prompt.generate_prompt(obj).replace("\n", " ")
