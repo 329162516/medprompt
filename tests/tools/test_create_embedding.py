2b54ece4fb64b8671e9145c7513ebbaed66e3b96
Bell Eapen <github@gulfdoctor.net>
2023-12-02 18:50:54 -0500

    Add test for creating embeddings and similarity search

--- tests/tools/test_create_embedding.py
+++ tests/tools/test_create_embedding.py
@@ -1,5 +1,8 @@
 import pytest
 from src.medprompt.tools.create_embedding import CreateEmbeddingFromFhirBundle
+import os
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
 
 def test_create_embedding_from_fhir_bundle():
     # Initialize the class
@@ -12,6 +15,18 @@ def test_create_embedding_from_fhir_bundle():
     # Test the _arun method
     result = create_embedding._arun(patient_id="45657")
     assert result is not None
+
+def test_created_embedding():
+    EMBED_MODEL = os.getenv("EMBED_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
+    embedding = HuggingFaceEmbeddings(model_name=EMBED_MODEL)
+    vectorstore = Chroma(collection_name="45657", persist_directory=os.getenv("CHROMA_DIR", "/tmp/chroma"), embedding_function=embedding)
+    retriever = vectorstore.as_retriever()
+    result = vectorstore.similarity_search("Body Temperature", k=10)
+    # print(result)
+    assert result is not None
+    retreived = retriever.get_relevant_documents("Body Temperature", k=10)
+    print(retreived)
+
 
 # def test_create_embedding_from_fhir_bundle_with_invalid_patient_id():
 #     # Initialize the class
